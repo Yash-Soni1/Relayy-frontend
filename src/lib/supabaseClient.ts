04b18d07f170172ca741ba604bdc6684ff60254d
Yash Soni <yash2165soni@gmail.com>
2025-09-21 23:42:22 +0530

    Second Commit: Authentication with Supabase

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-// Directly paste your Supabase project details here
-const SUPABASE_URL = "https://mrflauftglbwqiwfimjx.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1yZmxhdWZ0Z2xid3Fpd2ZpbWp4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgzODEwMTEsImV4cCI6MjA3Mzk1NzAxMX0.OEgg1GBbvY_8mqTkEkkA5xpQLPohIn3645RBAHvR0gE";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
